f69212f4797de5bf9a263e8728436d7fb534d542
Gary Paige <gary@theoryblister.com>
2018-02-07 22:43:08 -0600

    initial commit

---
+++ README.md
@@ -0,0 +1,9 @@
+# Plus Days Application
+
+
+## Tech Stack
+- ClojureScript
+- core.async - CSP
+- Reagent
+- Garden
+- Firebase 
---
+++ test/cljs/plus_days/app_test.cljs
@@ -0,0 +1,7 @@
+(ns plus_days.app-test
+  (:require-macros [cljs.test :refer [deftest testing is]])
+  (:require [cljs.test :as t]
+            [plus_days.app :as app]))
+
+(deftest test-arithmetic []
+  (is (= (+ 0.1 0.2) 0.3) "Something foul is a float."))
