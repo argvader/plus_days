0000abfc5e89777d99534d5ccc5d02809bc2bf9d
Gary Paige <gary.paige@asurion.com>
2018-03-29 06:56:38 -0500

    trying to add firebase

---
+++ src/cljs/plus_days/db/firebase.cljs
@@ -0,0 +1,85 @@
+(ns plus_days.db.firebase
+  (:require [com.degel.re-frame-firebase :as firebase]
+            [re-frame.core :as re-frame]
+            [re-frame.loggers :refer [console]]
+            [iron.re-utils :as re-utils :refer [sub2 <sub >evt]]
+            [iron.utils :refer [ci-includes? validate]]
+            [clojure.spec.alpha :as s]))
+
+(s/def ::uid string?)
+(s/def ::provider-data any?)
+(s/def ::display-name (s/nilable string?))
+(s/def ::photo-url (s/nilable string?))
+(s/def ::email string?)
+(s/def ::user (s/nilable
+               (s/keys :req-un [::uid ::provider-data ::display-name ::photo-url ::email])))
+
+(s/def ::db-keys (s/keys :req [::user]))
+
+(comment Need spec for db schema)
+
+(defonce timestamp-marker
+  (-> js/firebase .-database .-ServerValue .-TIMESTAMP))
+
+(defonce firebase-app-info
+  {:apiKey "AIzaSyDz_UFZOqejd7n-xlWWxqw3gBHe1kaki9o"
+   :authDomain "plusdays-4efdf.firebaseapp.com"
+   :databaseURL "https://plusdays-4efdf.firebaseio.com"
+   :storageBucket "plusdays-4efdf.appspot.com"})
+
+(defn private-fb-path
+  ([path]
+   (private-fb-path path (<sub [::uid])))
+  ([path uid]
+   (when uid
+     (into [:private uid] path))))
+
+(defn all-shared-fb-path
+  [path]
+  (into [:shared] path))
+
+
+(defn my-shared-fb-path
+  ([path]
+   (my-shared-fb-path path (<sub [::uid])))
+  ([path uid]
+   (when uid
+     (into [:shared (first path) uid] (rest path)))))
+
+(sub2 ::user [::user])
+(sub2 ::uid  [::user :uid])
+
+(re-frame/reg-event-fx
+ ::set-user
+ (fn [{db :db} [_ user]]
+   (into {:db (assoc db ::user user)}
+         (when user
+           {:firebase/write {:path       (my-shared-fb-path [:users-details] (:uid user))
+                             :value      (into {:timestamp timestamp-marker}
+                                               (select-keys user [:display-name :email :photo-url]))
+                             :on-success #(console :log "Logged in:" (:display-name user))
+                             :on-failure #(console :error "Login failure: " %)}}))))
+
+
+(re-frame/reg-event-fx
+ :sign-in
+ (fn [_ _] {:firebase/google-sign-in nil}))
+
+(re-frame/reg-event-fx
+ :sign-out
+ (fn [_ _] {:firebase/sign-out nil}))
+
+(re-frame/reg-event-fx
+ :firebase-error
+ (fn [_ [_ error]]
+   (js/console.error (str "FIREBASE ERROR:\n" error))))
+
+
+(defn logged-in? [db]
+  (some? (get-in db [::user :uid])))
+
+(defn ^:export init []
+  (firebase/init :firebase-app-info      firebase-app-info
+                 :get-user-sub           [::user]
+                 :set-user-event         [::set-user]
+                 :default-error-handler  [:firebase-error]))
