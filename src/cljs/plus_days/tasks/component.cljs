242dcc713465386baff949f64c844bff796797c8
Gary Paige <gary.paige@asurion.com>
2018-04-05 21:10:07 -0500

    spike setting

--- src/cljs/plus_days/tasks/component.cljs
+++ src/cljs/plus_days/tasks/component.cljs
@@ -1,8 +1,11 @@
 (ns plus_days.tasks.component
   (:require [plus_days.tasks.styles :refer [style]]
             [plus_days.tasks.subscriptions]
-            [re-frame.core :as re-frame :refer [subscribe]]))
+            [plus_days.tasks.events]
+            [re-frame.core :as re-frame :refer [subscribe dispatch]]))
 
 (defn component[]
   (let [data (subscribe [:tasks])]
-    [:div {:class-name (:container style)} (str "Tasks "  @data)]))
+    [:div {:class-name (:container style)}
+      (str "Tasks "  @data)
+      [:button {:on-click #(dispatch [:new-task "New Task"])} "Add"]]))
